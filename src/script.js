3d697d44c7c357e835c8b116c7243576b7976d0e
Nata <nata6nosenok@gmail.com>
2022-07-18 18:29:12 +0300

    add days

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
+function formatDay (timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+return day;  
+}
+
 function displayForecast (response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
@@ -6,7 +12,7 @@ function displayForecast (response) {
   forecastHtml = forecastHtml + `
       <div class="col-2">
           <div class="weather-forecast-date">
-          ${forecastDay.dt}
+          ${formatDay(forecastDay.dt)}
           </div>
       <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" id="icon" height="40px">
           <div class="weather-forecast-temperature">${Math.round(forecastDay.temp.max)} ℃ /<span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)} ℃</span> 
